2ca57af39d7c3351fbbab947a4aa0998d888e4ed
Gage Kolojaco <111771633+GageKolojaco@users.noreply.github.com>
2025-08-13 23:09:58 -0500

    added InventoryItem.java

---
+++ Inventory/InventoryItem.java
@@ -0,0 +1,20 @@
+package Inventory;
+
+import Produce.Produce;
+
+public class InventoryItem {
+    private Produce produce;
+    private int numberOfItems;
+
+    public InventoryItem(Produce produce, int numberOfItems){
+        this.produce = produce;
+        this.numberOfItems = numberOfItems;
+    }
+
+    public Produce getProduce(){return produce;}
+
+    public int getNumberOfItems(){return numberOfItems;}
+
+    public void setNumberOfItems(int numberOfItems){this.numberOfItems = numberOfItems;}
+
+}
---
+++ Stand/Stand.java
@@ -0,0 +1,5 @@
+package Stand;
+
+public class Stand {
+    
+}
