e0f83b5b35184d6fd959a0ef0c28ca072332c572
Gage Kolojaco <111771633+GageKolojaco@users.noreply.github.com>
2025-08-14 00:02:23 -0500

    added adStand()

--- Market/Market.java
+++ Market/Market.java
@@ -6,9 +6,36 @@ public class Market {  //using a basic linked list to connect all the stands in 
 
     private static class Node { //nested node class
         Stand stand;
+        int standID;
         Node next;
 
-        Node (Stand stand)
+        Node(Stand stand) {
+            this.stand = stand;
+            this.standID = stand.getStandID();
+            this.next = null;
+        }
     }
+
+    private Node head;
+    private int size;
+
+    public Market(){
+        this.head = null;
+        this.size = 0;
+    }
+
+    public void addStand(Stand stand){
+        Node newNode = new Node(stand);
+        if (head == null){
+            head = newNode;
+        } else {
+            Node currentNode = head;
+            while (currentNode.next != null){currentNode = currentNode.next;}
+            currentNode.next = newNode;
+
+        }
+        size++;
+    }
+
     
 }
