bd00d5b2cae8a6c2c9a52c072db3cae7f27e7afa
Gage Kolojaco <111771633+GageKolojaco@users.noreply.github.com>
2025-08-13 23:34:19 -0500

    completed Stand.java for now

---
+++ Stand/Stand.java
@@ -0,0 +1,45 @@
+package Stand;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import Farmer.Farmer;
+import Inventory.InventoryItem;
+import Produce.Produce;
+
+public class Stand {
+    private Farmer farmer;
+    private List<InventoryItem> inventory;
+
+    public Stand (Farmer farmer){ //Stand constructor
+        this.farmer = farmer; // we will see how we want to implement this
+                              // thinking about making the constructor only use the list
+        this.inventory = new ArrayList<>();
+    }
+    
+    public Farmer getFarmer(){return farmer;}
+
+    public void setFarmer(Farmer farmer){this.farmer = farmer;}
+
+    public void addProduce(Produce produce, int numberOfItems){
+        for (InventoryItem item : inventory){//search through existing inventory
+            if (item.getProduce().equals(produce)){ //if the produce already exists then add more
+                item.setNumberOfItems(item.getNumberOfItems()+numberOfItems);
+                return;
+            }
+        }
+        InventoryItem newItem = new InventoryItem(produce, numberOfItems); //else it will hit this statement and add the new item
+        inventory.add(newItem);
+    }
+
+    public void removeProduce(Produce produce, int numberOfItems){
+        for (InventoryItem item : inventory){
+            if (item.getProduce().equals(produce)){
+                item.setNumberOfItems(item.getNumberOfItems()-numberOfItems);
+            }
+        }
+    }
+
+    public List<InventoryItem> getIventory(){return inventory;}
+
+}
