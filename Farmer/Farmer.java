a8500e7bd913ef90626ab520ace6f6cd44b3187e
Gage Kolojaco <111771633+GageKolojaco@users.noreply.github.com>
2025-08-13 22:46:52 -0500

    added farmer class

---
+++ Farmer/Farmer.java
@@ -0,0 +1,12 @@
+package Farmer;
+
+public class Farmer {
+    private String name;
+
+    public Farmer(String name){
+        this.name = name;
+    }
+    public String getName(){return name;}
+
+    public void setName(String name){this.name = name;} 
+}
--- Produce/Produce.java
+++ Produce/Produce.java
@@ -3,12 +3,12 @@ public abstract class Produce {
     private String name;
     private double unitPrice;
 
-    public Produce (String name, double unitPrice){ //Constructor
+    public Produce(String name, double unitPrice){ //Constructor
         this.name = name;
         this.unitPrice = unitPrice;
     }
 
-    public String getName(){return name;}
+    public String getName(){return name;} //Getters and setters
 
     public double getPrice(){return unitPrice;}
 
