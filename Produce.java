5a1d8ca7cfd598132aeefa24e66ccda1d04609e1
Gage Kolojaco <111771633+GageKolojaco@users.noreply.github.com>
2025-08-13 22:28:38 -0500

    added abstract produce parent class

---
+++ Produce.java
@@ -0,0 +1,16 @@
+public abstract class Produce {
+    private String name;
+    private double unitPrice;
+
+    public Produce (String name, double unitPrice){ //Constructor
+        this.name = name;
+        this.unitPrice = unitPrice;
+    }
+
+    public String getName(){return name;}
+
+    public double getPrice(){return unitPrice;}
+
+    public void setPrice(double unitPrice){this.unitPrice = unitPrice;}
+    
+}
