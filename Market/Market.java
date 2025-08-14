34090f1cf6623f94889e226f5e5c31b08bd5fb98
Gage Kolojaco <111771633+GageKolojaco@users.noreply.github.com>
2025-08-13 23:41:34 -0500

    going back to work on stand.java

---
+++ Market/Market.java
@@ -0,0 +1,14 @@
+package Market;
+
+import Stand.Stand;
+
+public class Market {  //using a basic linked list to connect all the stands in the market
+
+    private static class Node { //nested node class
+        Stand stand;
+        Node next;
+
+        Node (Stand stand)
+    }
+    
+}
