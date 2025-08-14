65dc574dc3d11449ee00e3a447407582a1a9840c
Gage Kolojaco <111771633+GageKolojaco@users.noreply.github.com>
2025-08-14 13:24:39 -0500

    added a bit of user proofing

--- main.java
+++ main.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import Market.Market;
@@ -12,14 +13,21 @@ public class Main {
             System.out.println("Main Menu:");
             System.out.println("1:> Open Market");
             System.out.println("2:> Exit");
-            int choice = input.nextInt();
-            switch(choice){
-                case 1:
-                    openMarket();
-                case 2:
-                    mainMenu = false;
-                default:
-                    System.out.println("Please enter the value 1 or 2");
+            try{    
+                int choice = input.nextInt();
+                input.nextLine();
+                switch(choice){
+                    case 1:
+                        openMarket();
+                    case 2:
+                        mainMenu = false;
+                    default:
+                        System.out.println("Please enter the integer value 1 or 2");
+                }
+            }
+            catch (InputMismatchException e) {
+                System.out.println("Please input a valid integer.");
+                input.nextLine();
             }
         }
     }
@@ -34,19 +42,27 @@ public class Main {
             System.out.println("3:> Assign Produce to Stand");
             System.out.println("4:> View Stands");
             System.out.println("5:> Search by Produce Name");
-            int choice = input.nextInt();
-            switch(choice){
-                case 1:
-                //addStand();
-                case 2:
-                //assignFarmerToStand();
-                case 3:
-                //assignProduce to Stand();
-                case 4:
-                //viewStands();
-                case 5:
-                //search();
-
+            try{    
+                int choice = input.nextInt();
+                input.nextLine();
+                switch(choice){
+                    case 1:
+                    //addStand();
+                    case 2:
+                    //assignFarmerToStand();
+                    case 3:
+                    //assignProduce to Stand();
+                    case 4:
+                    //viewStands();
+                    case 5:
+                    //search();
+                    default:
+                        System.out.println("Please enter an integer value 1 through 5");
+                }
+            }
+            catch (InputMismatchException e) {
+                System.out.println("Please input a valid integer.");
+                input.nextLine();
             }
         }
     }
