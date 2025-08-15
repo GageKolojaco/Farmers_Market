d80eefc9a30295c52a8edf33d6fbbed45a9da6aa
Gage Kolojaco <111771633+GageKolojaco@users.noreply.github.com>
2025-08-14 23:49:56 -0500

    intial factory

--- Inventory/Produce/ProduceFactory.java
+++ Inventory/Produce/ProduceFactory.java
@@ -1,5 +1,25 @@
 package Inventory.Produce;
 
+import Inventory.InventoryItem;
+
 public class ProduceFactory {
-    
+    private ProduceFactory(){}
+
+    public static InventoryItem build(String name, double unitPrice, int numberOfItems) throws Exception{
+        String produceName = name.trim().toLowerCase(); //trimming for consistency
+        switch (produceName) {
+            case "asparagus":
+                return new Asparagus(unitPrice, numberOfItems);
+            case "celery":
+                return new Asparagus(unitPrice, numberOfItems);
+            case "dragonfruit":
+                return new Asparagus(unitPrice, numberOfItems);
+            case "strawberry":
+                return new Asparagus(unitPrice, numberOfItems);
+            case "watermelon":
+                return new Asparagus(unitPrice, numberOfItems);
+            default:
+                throw new Exception();
+        }
+    }
 }
